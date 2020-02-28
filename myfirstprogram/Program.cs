fc55495a2a3c24d4b0b2be8bba6996dec647ed06
103046492 <61527375+103046492@users.noreply.github.com>
2020-02-28 16:32:54 +1100

    Add files via upload

---
+++ myfirstprogram/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace myfirstprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name");
+            string firstname = Console.ReadLine();
+            
+            Console.WriteLine("Please enter your surname");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Please enter your studentID");
+            string studentID = Console.ReadLine();
+
+            Console.WriteLine("Please enter your year of birth");
+            string yob = Console.ReadLine();
+
+            Console.WriteLine("What year is it?");
+            string currentyear = Console.ReadLine();
+
+            int num1 = int.Parse(currentyear);
+
+            int num2 = int.Parse(yob);
+
+            int age = num1 - num2;
+
+            string message = "Hello " + firstname + " " + surname + "!  " + age.ToString() + " " + studentID;
+            
+            Console.WriteLine(message);
+            
+            Console.ReadKey();
+        }
+    }
+}
